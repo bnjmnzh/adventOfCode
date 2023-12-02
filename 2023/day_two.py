259f3a5fc82bb8560f3559f4213814bb9d01d5e7
Benjamin Zhuo <benjamin.zhuo@yahoo.com>
2023-12-02 18:31:33 -0500

    day 2 part one

---
+++ 2023/day_two.py
@@ -0,0 +1,38 @@
+import re
+
+RED = r'\d+(?= red)'
+GREEN = r'\d+(?= green)'
+BLUE = r'\d+(?= blue)'
+
+MAX_RED = 12
+MAX_GREEN = 13
+MAX_BLUE = 14
+
+
+def get_round_counts(round):
+    red_match = re.search(RED, round)
+    blue_match = re.search(BLUE, round)
+    green_match = re.search(GREEN, round)
+    red = 0 if not red_match else int(red_match.group(0))
+    green = 0 if not green_match else int(green_match.group(0))
+    blue = 0 if not blue_match else int(blue_match.group(0))
+    return red, green, blue
+
+
+def verify_scores(red, green, blue):
+    return red <= MAX_RED and blue <= MAX_BLUE and green <= MAX_GREEN
+
+
+def parse_line_1(line):
+    game_num, scores = line.split(': ')
+    idx = game_num.split(' ')[1]
+    rounds = scores.split('; ')
+    counts = [get_round_counts(x) for x in rounds]
+    return int(idx) if all(verify_scores(*c) for c in counts) else 0
+
+if __name__ == '__main__':
+    with open('./inputs/2.txt', 'r') as f:
+        sum = 0
+        for line in f:
+            sum += parse_line_1(line)
+        print(f'Sum of possible game indexes: {sum}')
