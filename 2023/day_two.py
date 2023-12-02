0b3032247c066b6d055a773dfeec372337dd2a57
Benjamin Zhuo <benjamin.zhuo@yahoo.com>
2023-12-02 18:43:10 -0500

    day 2 part two

--- 2023/day_two.py
+++ 2023/day_two.py
@@ -30,9 +30,20 @@ def parse_line_1(line):
     counts = [get_round_counts(x) for x in rounds]
     return int(idx) if all(verify_scores(*c) for c in counts) else 0
 
+def parse_line_2(line):
+    game_num, scores = line.split(': ')
+    idx = game_num.split(' ')[1]
+    rounds = scores.split('; ')
+    counts = [get_round_counts(x) for x in rounds]
+    min_red = max([c[0] for c in counts])
+    min_green = max([c[1] for c in counts])
+    min_blue = max([c[2] for c in counts])
+    return min_red * min_green * min_blue
+
+
 if __name__ == '__main__':
     with open('./inputs/2.txt', 'r') as f:
         sum = 0
         for line in f:
-            sum += parse_line_1(line)
+            sum += parse_line_2(line)
         print(f'Sum of possible game indexes: {sum}')
