12689f5b45251116af4d11c9a09e6925486cfdce
Benjamin Zhuo <benjamin.zhuo@yahoo.com>
2023-12-02 17:36:51 -0500

    day one solution

--- 2023/day_one.py
+++ 2023/day_one.py
@@ -1,16 +1,48 @@
 import re
 
-def parse_input(input):
+
+INT_MAP = {
+    'one': 1,
+    'two': 2,
+    'three': 3,
+    'four': 4,
+    'five': 5,
+    'six': 6,
+    'seven': 7,
+    'eight': 8,
+    'nine': 9,
+    '1': 1,
+    '2': 2,
+    '3': 3,
+    '4': 4,
+    '5': 5,
+    '6': 6,
+    '7': 7,
+    '8': 8,
+    '9': 9
+}
+
+
+def parse_input_1(input):
     sum = 0
     for line in input:
-        callibration_values = re.findall(r'\d', line)
-        print(callibration_values)
-        sum += int(callibration_values[0]) * 10 + int(callibration_values[-1])
+        values = re.findall(r'\d', line)
+        sum += int(values[0]) * 10 + int(values[-1])
     print(f'Sum of callibration values: {sum}')
     return sum
 
 
+def parse_input_2(input):
+    sum = 0
+    for line in input:
+        values = re.findall(r'(?=(one|two|three|four|five|six|seven|eight|nine|\d))', line)
+        print(values)
+        sum += INT_MAP[values[0]] * 10 + INT_MAP[values[-1]]
+    print(f'Sum of callibration values: {sum}')
+    return sum
+
 
 if __name__ == '__main__':
     with open('./inputs/1.txt', 'r') as f:
-        parse_input(f)
+        # parse_input_1(f)
+        parse_input_2(f)
